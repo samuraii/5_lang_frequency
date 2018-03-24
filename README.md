9d03482b84a68b718a453c07e0aeeb2bd7d108d6
campside <chirkovv.ma@yandex.ru>
2018-03-24 22:55:09 +0300

    Chznges

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # Frequency Analysis of Words
 
-This program allows to analyze word frequency in text file. Can be used with any language as long as text file is encoding is 'utf-8'
+This program allows to analyze word frequency in text file. 
+Can be used with any language as long as text file is encoding is 'utf-8'
 
 # Example of usage
 
 ```python
 import lang_frequency as lf
 
-text_from_file = lf.load_data('Hamlet.txt')
+text_from_file = lf.load_data()
 print(lf.get_most_frequent_words(text_from_file))
 ```
 
 Output:
 ```bash
+Input path to file: Hamlet.txt
 {'the': 1101, 'and': 912, 'to': 752, 'of': 692, 'a': 542, 'i': 532, 'you': 512, 'my': 503, 'in': 401, 'it': 368}
 ```
 # Project Goals
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,17 +1,17 @@
 
-# -*- coding: utf-8 -*-
 from collections import Counter
 
 
-def load_data(filepath):
-    with open(filepath, 'r') as f:
+def load_text_file():
+    path_to_text_file = input('Input path to file: ')
+    with open(path_to_text_file, 'r') as f:
         return f.read()
 
 
-def get_most_frequent_words(text):
-    return dict(Counter(text.lower().split()).most_common(10))
+def get_most_frequent_words(text, counter):
+    return dict(Counter(text.lower().split()).most_common(counter))
 
 
 if __name__ == '__main__':
-    text_from_file = load_data('Hamlet.txt')
-    print(get_most_frequent_words(text_from_file))
+    text_from_file = load_text_file()
+    print(get_most_frequent_words(text_from_file, 10))
