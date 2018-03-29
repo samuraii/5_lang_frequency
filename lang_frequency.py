b3fc7afa8b9127638719a0c7a42d5172ac8a3769
campside <chirkovv.ma@yandex.ru>
2018-03-29 21:05:01 +0300

    Правки по ревью
    1) Вынес обработку из из функции которая только читает данные. 2) Поправил вывод чтобы начинался с 1. 3) Переименовал функции вывода результата и удалению знаков пунктуации.  4) Добавил проверку на неверную кодировку файла.

--- lang_frequency.py
+++ lang_frequency.py
@@ -5,21 +5,21 @@ from collections import Counter
 
 def load_text_file(path_to_file):
     with open(path_to_file, 'r') as file_data:
-        return file_data.read().lower()
+        return file_data.read()
 
 
-def filter_text(raw_text):
+def remove_punctuation(raw_text):
     return re.sub(r'[^\w\s]', '', raw_text)
 
 
 def get_most_frequent_words(text, words_count=10):
-    return Counter(text.split()).most_common(words_count)
+    return Counter(text.lower().split()).most_common(words_count)
 
 
-def display_words(words):
+def print_words(words):
     print('#', 'count', 'word')
     for number, (word, amount) in enumerate(words):
-        print('{0:<1} {1:^6} {2}'.format(number, word, amount))
+        print('{0:<1} {1:^6} {2}'.format(number + 1, word, amount))
 
 if __name__ == '__main__':
     try:
@@ -28,7 +28,9 @@ if __name__ == '__main__':
         exit('File not passed to script.')
     except FileNotFoundError:
         exit('File not found.')
+    except UnicodeDecodeError:
+        exit('File encoding is not UTF-8')
 
-    filtered_text = filter_text(raw_text)
-    words_count = get_most_frequent_words(filtered_text)
-    display_words(words_count)
+    text_without_punctuation = remove_punctuation(raw_text)
+    words_count = get_most_frequent_words(text_without_punctuation)
+    print_words(words_count)
